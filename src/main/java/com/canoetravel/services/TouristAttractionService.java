40d8ef9def5965ef956637134440f6ad2808d539
aliaksei ivanchyk <alexivacnhyk@gmail.com>
2021-06-02 07:55:38 -0600

    added tourist service

---
+++ src/main/java/com/canoetravel/services/TouristAttractionService.java
@@ -0,0 +1,13 @@
+package com.canoetravel.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TouristAttractionService {
+
+	public TouristAttractionService() {
+	}
+	
+	
+
+}
