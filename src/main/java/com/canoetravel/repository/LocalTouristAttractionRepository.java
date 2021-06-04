b6479eee8b3c5658e839be99c7d82542ae98a49e
devaraj1234 <dev.acharya286@gmail.com>
2021-06-03 23:29:24 -0500

    created local tourist attraction places repository

---
+++ src/main/java/com/canoetravel/repository/LocalTouristAttractionRepository.java
@@ -0,0 +1,9 @@
+package com.canoetravel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.canoetravel.entities.LocalTouristAttraction;
+
+public interface LocalTouristAttractionRepository extends JpaRepository<LocalTouristAttraction, Integer> {
+
+}
