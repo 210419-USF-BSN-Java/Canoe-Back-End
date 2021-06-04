3cfd7a405936de614e3c6bdb76292017b62d7ba1
devaraj1234 <dev.acharya286@gmail.com>
2021-06-03 23:42:44 -0500

    created local tourist attraction places service class

--- src/main/java/com/canoetravel/services/TouristAttractionService.java
+++ src/main/java/com/canoetravel/services/TouristAttractionService.java
@@ -1,13 +1,23 @@
 package com.canoetravel.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.canoetravel.entities.LocalTouristAttraction;
+import com.canoetravel.repository.LocalTouristAttractionRepository;
 
 @Service
 public class TouristAttractionService {
 
-	public TouristAttractionService() {
+	private LocalTouristAttractionRepository touristAttractionRepo;
+
+	@Autowired
+	public TouristAttractionService(LocalTouristAttractionRepository touristAttractionRepo) {
+		this.touristAttractionRepo = touristAttractionRepo;
 	}
-	
-	
+
+	public LocalTouristAttraction saveLocalTouristAttraction(LocalTouristAttraction localTouristAttraction) {
+		return touristAttractionRepo.save(localTouristAttraction);
+	}
 
 }
