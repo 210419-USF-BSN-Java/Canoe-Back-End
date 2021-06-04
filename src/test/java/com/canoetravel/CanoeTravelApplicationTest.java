33be2a29780be1047902382667f70d1004c04312
devaraj1234 <dev.acharya286@gmail.com>
2021-06-04 00:09:38 -0500

    added test file

---
+++ src/test/java/com/canoetravel/CanoeTravelApplicationTest.java
@@ -0,0 +1,13 @@
+package com.canoetravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CanoeTravelApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
