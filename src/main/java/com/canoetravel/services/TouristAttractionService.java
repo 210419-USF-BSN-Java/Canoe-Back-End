871d381d1055e622d1a12d7bd7dd8ae740b5b08d
devaraj1234 <dev.acharya286@gmail.com>
2021-06-04 15:26:30 -0500

    added junit test cases for user service class

--- src/main/java/com/canoetravel/services/TouristAttractionService.java
+++ src/main/java/com/canoetravel/services/TouristAttractionService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.canoetravel.entities.Destination;
 import com.canoetravel.entities.LocalTouristAttraction;
 import com.canoetravel.repository.LocalTouristAttractionRepository;
 
--- src/test/java/com/canoetravel/serviceTest/UserServiceTest.java
+++ src/test/java/com/canoetravel/serviceTest/UserServiceTest.java
@@ -1,11 +1,11 @@
 package com.canoetravel.serviceTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -19,17 +19,83 @@ import com.canoetravel.services.UserService;
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
 	
+	private static User user1, user2;
+	private static List<User> userList;
+	
 	@Mock
-	private UserService userServ;
+	private static UserService userServ;
+	
+	
+	@BeforeClass
+	public static void setup() {
+		userServ = Mockito.mock(UserService.class);
+		userList = new ArrayList<>();
+		user1 = new User(1, "testfname", "testlname", "testemail", "testlogin","testloginpassword",true, new UserRole());
+		user2 = new User(2, "testfname1", "testlname1", "testemail1", "testlogin1","testloginpassword1",true, new UserRole());
+		userList.add(user1);
+		userList.add(user2);	
+	}
 
 	@Test
-	public void getAllUsersUserTest() {
-		userServ = Mockito.mock(UserService.class);
-		List<User> userList = new ArrayList<>();
-		User user1 = new User(1, "Dev", "Acharya", "email", "dev.acharya","password",true, new UserRole());
-		userList.add(user1);
+	public void registerUserTest() {
 		Mockito.when(userServ.registerUser(user1)).thenReturn(true);
 		assertEquals(true, userServ.registerUser(user1));
+	}
+	
+	@Test
+	public void registerUserTest1() {
+		Mockito.when(userServ.registerUser(null)).thenReturn(false);
+		assertEquals(false, userServ.registerUser(null));
+	}
+	
+	@Test
+	public void getAllUser() {
+		Mockito.when(userServ.getAllUsers()).thenReturn(userList);
+		assertEquals(2, userServ.getAllUsers().size());
+	}
+	
+	@Test
+	public void authenticateLoginTest() {
+		Mockito.when(userServ.authenticateLogin(user1.getUserLogin(), user1.getUserLoginPassword())).thenReturn(user1);
+		assertEquals(user1, userServ.authenticateLogin(user1.getUserLogin(), user1.getUserLoginPassword()));
+	}
+	
+	@Test
+	public void authenticateLoginTest1() {
+		Mockito.when(userServ.authenticateLogin(user2.getUserLogin(), user2.getUserLoginPassword())).thenReturn(user2);
+		assertEquals(user2, userServ.authenticateLogin(user2.getUserLogin(), user2.getUserLoginPassword()));
+	}
+	
+	@Test
+	public void authenticateLoginTest2() {
+		Mockito.when(userServ.authenticateLogin("wronglogin", "wrongpassword")).thenReturn(null);
+		assertEquals(null, userServ.authenticateLogin("wronglogin", "wrongpassword"));
+	}
+	
+	@Test
+	public void updateUserInfoTest() {
+		user1.setUserEmail("updatetest@emai.com");
+		Mockito.when(userServ.updateUserInfo(user1)).thenReturn(user1);
+		assertEquals("updatetest@emai.com", userServ.updateUserInfo(user1).getUserEmail());
+	}
+	
+	@Test
+	public void updateUserInfoTest1() {
+		user2.setUserFname("updateUserFnameTest");
+		Mockito.when(userServ.updateUserInfo(user2)).thenReturn(user2);
+		assertEquals("updateUserFnameTest", userServ.updateUserInfo(user2).getUserFname());
+	}
+	
+	@Test
+	public void findByEmailTest() {
+		Mockito.when(userServ.findByEmail(user2.getUserEmail())).thenReturn(user2);
+		assertEquals(user2, userServ.findByEmail("testemail1"));
+	}
+	
+	@Test
+	public void findByEmailTest1() {
+		Mockito.when(userServ.findByEmail("wrongEmail")).thenReturn(null);
+		assertEquals(null, userServ.findByEmail("wrongEmail"));
 	}
 
 }
